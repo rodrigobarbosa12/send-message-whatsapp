593fa8ca2cdf19f80abd2a09810b25c7fa1ae740
rodrigobarbosa12 <barbosa@maxscalla.com.br>
2021-12-07 18:43:38 -0300

    :rocket: Send message Whatsapp :cactus:

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,10 @@ import {
   Button,
   Image,
   Alert,
+  View,
+  TouchableOpacity,
 } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 import {
   Container,
   Form,
@@ -33,38 +36,59 @@ const SendMessageToNumber = (): ReactElement => {
     Linking.openURL(`whatsapp://send?text=${message}&phone=+55${numberWithoutMask}`);
   };
 
-  return (
-    <Container>
-      <Image source={Logo} style={{ width: 80, height: 80, marginBottom: 50 }} />
-      <Form>
-        <FormInput
-          icon="mail-outline"
-          keyboardType="default"
-          placeholder="Olá"
-          placeholderTextColor="#999"
-          onChangeText={(value: string) => setMassage(value)}
-        />
+  const openGithub = () => Linking.openURL('https://github.com/rodrigobarbosa12');
+  const openLinkedin = () => Linking.openURL('https://www.linkedin.com/in/rodrigo-barbosa-7a1429157/');
 
-        <InputMaskPhone
-          icon="phone"
-          type="cel-phone"
-          options={{
-            maskType: 'BRL',
-            withDDD: true,
-            dddMask: '(99) ',
-          }}
-          value={number}
-          placeholder="Ex: (11) 91234-5678"
-          placeholderTextColor="#999"
-          onChangeText={(value: string) => setNumber(value)}
-        />
-        <Button
-          title="Enviar"
-          onPress={openWhatsapp}
-          color="#484848"
-        />
-      </Form>
-    </Container>
+  return (
+    <>
+      <Container>
+        <Image source={Logo} style={{ width: 80, height: 80, marginBottom: 50 }} />
+        <Form>
+          <FormInput
+            icon="mail-outline"
+            keyboardType="default"
+            placeholder="Olá"
+            placeholderTextColor="#999"
+            onChangeText={(value: string) => setMassage(value)}
+          />
+
+          <InputMaskPhone
+            icon="phone"
+            type="cel-phone"
+            options={{
+              maskType: 'BRL',
+              withDDD: true,
+              dddMask: '(99) ',
+            }}
+            value={number}
+            placeholder="Ex: (11) 91234-5678"
+            placeholderTextColor="#999"
+            onChangeText={(value: string) => setNumber(value)}
+          />
+          <Button
+            title="Enviar"
+            onPress={openWhatsapp}
+            color="#484848"
+          />
+        </Form>
+      </Container>
+      <View style={{
+        backgroundColor: '#000',
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+        paddingBottom: 30,
+      }}
+      >
+        <View />
+        <TouchableOpacity onPress={openGithub}>
+          <MaterialCommunityIcons name="github" color="#999" size={40} />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={openLinkedin}>
+          <MaterialCommunityIcons name="linkedin" color="#999" size={40} />
+        </TouchableOpacity>
+        <View />
+      </View>
+    </>
   );
 };
 
